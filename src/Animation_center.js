9244921ea41e6a7e0ea397dd992f51744fb53dbb
AhmmedHesham <163453118+AhmmedHesham@users.noreply.github.com>
2024-04-30 04:58:09 +0300

    formatting the code

--- src/Animation_center.js
+++ src/Animation_center.js
@@ -4,28 +4,20 @@ export const createSphere = (id, position, delay) => (
     <>
         <Sphere id={id} position={position} delay={delay} />
         <Sphere id={id} position={[position[0] +2, position[1] , position[2]]} delay={delay+ 1000} />
-
         <Sphere id={id} position={[position[0] , position[1] -2, position[2]]} delay={delay + 4000} />
         <Sphere id={id} position={[position[0] +2, position[1] -2, position[2]]} delay={delay + 4000} />
-
         <Sphere id={id} position={[position[0] +8, position[1] , position[2]]} delay={delay+ 7000} />
         <Sphere id={id} position={[position[0] +10, position[1] , position[2]]} delay={delay+ 7000} />
-
         <Sphere id={id} position={[position[0] +8, position[1] -2, position[2]]} delay={delay + 7000} />
-        <Sphere id={id} position={[position[0] +10, position[1] -2, position[2]]} delay={delay + 7000} />
-
-
-        
+        <Sphere id={id} position={[position[0] +10, position[1] -2, position[2]]} delay={delay + 7000} />    
     </>
   );
-  
   export const createArrowHorizontal = (id, delay, start, end) => (
     <>
       <CustomArrowHorizontal id={id} delay={delay} start={start} end={end} />
       <CustomArrowHorizontal id={id} delay={delay + 2000} start={[start[0], 0, start[2]]} end={[end[0], 0, end[2]]} />
       <CustomArrowHorizontal id={id} delay={delay + 5000} start={[-start[0]+2, 2, start[2]]} end={[-end[0] +2, 2, end[2]]} />
       <CustomArrowHorizontal id={id} delay={delay + 5000} start={[-start[0] +2, 0, start[2]]} end={[-end[0] +2, 0, end[2]]} />
-      
     </>
   );
   export const createArrowVertical = (id, delay, start, end) => (
@@ -36,7 +28,6 @@ export const createSphere = (id, position, delay) => (
       <CustomArrowVertical id={`${id}_4`} delay={delay + 3000} start={[-start[0] + 2, start[1], start[2]]} end={[-end[0] + 2, end[1], end[2]]} />
     </>
   );
-  
   export const createArrowDiagonal = (id, delay, start, end,type='diagonal') => (
     <>
       <CustomArrowDiagonal id={`${id}_1`} delay={delay} start={start} end={end} type={type} />
@@ -45,18 +36,14 @@ export const createSphere = (id, position, delay) => (
       <CustomArrowDiagonal id={`${id}_4`} delay={delay + 3000} start={[-start[0] + 2, start[1], start[2]]} end={[-end[0] + 2, end[1], end[2]]} type={"diagonal2"} />
     </>
   );
-  
   export const createBottomUpTopDown = (id, delay, start, end, type='Bottom_UP') => (
     <>
       <CustomArrowTopUPbottomDown id={id} delay={delay} start={[start[0] +1, start[1], start[2]]} end={[end[0]+1, end[1], end[2]]}  type={type} />
       <CustomArrowTopUPbottomDown id={id} delay={delay} start={[start[0] -1, start[1], start[2]]} end={[end[0]- 1, end[1], end[2]]}  type={type} />
       <CustomArrowTopUPbottomDown id={id} delay={delay} start={[start[0] +1, start[1]+2, start[2]]} end={[end[0]+1, end[1]+2, end[2]]}  type={type} />
       <CustomArrowTopUPbottomDown id={id} delay={delay} start={[start[0] -1, start[1]+2, start[2]]} end={[end[0]- 1, end[1]+2, end[2]]}  type={type} />
-      
       <CustomArrowTopUPbottomDown id={`${id}_1`} delay={delay}  start={[-start[0] -1, start[1], start[2]]} end={[-end[0]-1, end[1], end[2]]} type={'top_DOWN'} />
-  <CustomArrowTopUPbottomDown id={`${id}_2`} delay={delay} start={[-start[0] -1, start[1]+2, start[2]]} end={[-end[0]-1, end[1]+2, end[2]]} type={'top_DOWN'} />
-       
-            
+      <CustomArrowTopUPbottomDown id={`${id}_2`} delay={delay} start={[-start[0] -1, start[1]+2, start[2]]} end={[-end[0]-1, end[1]+2, end[2]]} type={'top_DOWN'} />    
       <CustomArrowTopUPbottomDown id={`${id}_3`}delay={delay} start={[-start[0]+1, start[1]+2, start[2]]} end={[-end[0]+1, end[1]+2, end[2]]} type={'top_DOWN'} />
       <CustomArrowTopUPbottomDown id={`${id}_4`} delay={delay } start={[-start[0] + 1, start[1], start[2]]} end={[-end[0] + 1, end[1], end[2]]} type={'top_DOWN'} /> 
   
--- src/App.js
+++ src/App.js
@@ -3,36 +3,21 @@ import { createSphere, createArrowHorizontal, createArrowVertical, createArrowDi
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 
-
 export default function App() {
   return (
     <Canvas style={{ background: '#a8a7b5' }} camera={{ type: 'orthographic', position: [0, 0.5, 10] }}>
       <ambientLight intensity={Math.PI / 2} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-
       <group position={[-1, 0, 0]}>
         {createSphere("entity1", [-4, 2, 0], 1000)}
-        {/* {createSphere("entity2", [-2, 2, 0], 2000)}
-
-        
-        {createSphere("entity3", [-4, 0, 0], 5000)}
-        {createSphere("entity4", [-2, 0, 0], 5000)} */}
         <Circle id="circle1" position={[-3, 1, -1.5]} delay={6000} />
-
         {createArrowHorizontal("arrow1 and arrow4", 3000, [-4, 2, 0], [-2, 2, 0])}
         {createArrowVertical("arrow2 and arrow3", 5000, [-4, 0.5, 0], [-4, 1.5, 0])}
         {createArrowDiagonal("arrow5", 5000, [-2, 0, 0], [-4, 2, 0])}
-        {/* ... */}
       </group>
-
       <group position={[1, 0, 0]}>
-
-
-        
-
         <Circle id="circle1_1" position={[3, 1, -1.5]} delay={8000} />
-        {/* ... */}
       </group>
 
       <CustomArrowHorizontal id="arrow5" delay={11000} start={[-1, 1, -1.5]} end={[1, 1, -1.5]} />
