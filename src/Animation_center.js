84fec4e2a075f969edb7f0b6bafd7756e426a6df
AhmmedHesham <163453118+AhmmedHesham@users.noreply.github.com>
2024-04-30 04:42:33 +0300

    add Animation center and new arrows style

---
+++ src/Animation_center.js
@@ -0,0 +1,53 @@
+import { Sphere, CustomArrowHorizontal, CustomArrowVertical, CustomArrowDiagonal, CustomArrowTopUPbottomDown } from './components';
+
+export const createSphere = (id, position, delay) => (
+    <>
+      <Sphere id={id} position={position} delay={delay} />
+        {id}
+    </>
+  );
+  
+  export const createArrowHorizontal = (id, delay, start, end) => (
+    <>
+      <CustomArrowHorizontal id={id} delay={delay} start={start} end={end} />
+      <CustomArrowHorizontal id={id} delay={delay + 2000} start={[start[0], 0, start[2]]} end={[end[0], 0, end[2]]} />
+      <CustomArrowHorizontal id={id} delay={delay + 5000} start={[-start[0]+2, 2, start[2]]} end={[-end[0] +2, 2, end[2]]} />
+      <CustomArrowHorizontal id={id} delay={delay + 5000} start={[-start[0] +2, 0, start[2]]} end={[-end[0] +2, 0, end[2]]} />
+      
+    </>
+  );
+  export const createArrowVertical = (id, delay, start, end) => (
+    <>
+      <CustomArrowVertical id={`${id}_1`} delay={delay} start={start} end={end} />
+      <CustomArrowVertical id={`${id}_2`} delay={delay} start={[start[0] + 2, start[1], start[2]]} end={[end[0] + 2, end[1], end[2]]} />
+      <CustomArrowVertical id={`${id}_3`} delay={delay + 3000} start={[-start[0], start[1], start[2]]} end={[-end[0], end[1], end[2]]} />
+      <CustomArrowVertical id={`${id}_4`} delay={delay + 3000} start={[-start[0] + 2, start[1], start[2]]} end={[-end[0] + 2, end[1], end[2]]} />
+    </>
+  );
+  
+  export const createArrowDiagonal = (id, delay, start, end,type='diagonal') => (
+    <>
+      <CustomArrowDiagonal id={`${id}_1`} delay={delay} start={start} end={end} type={type} />
+      <CustomArrowDiagonal id={`${id}_2`} delay={delay} start={[start[0] , start[1], start[2]]} end={[end[0] , end[1], end[2]]} type={"diagonal2"} />
+      <CustomArrowDiagonal id={`${id}_3`} delay={delay + 3000} start={[-start[0] +2, start[1], start[2]]} end={[-end[0] + 2, end[1], end[2]]} type={type} />
+      <CustomArrowDiagonal id={`${id}_4`} delay={delay + 3000} start={[-start[0] + 2, start[1], start[2]]} end={[-end[0] + 2, end[1], end[2]]} type={"diagonal2"} />
+    </>
+  );
+  
+  export const createBottomUpTopDown = (id, delay, start, end, type='Bottom_UP') => (
+    <>
+      <CustomArrowTopUPbottomDown id={id} delay={delay} start={[start[0] +1, start[1], start[2]]} end={[end[0]+1, end[1], end[2]]}  type={type} />
+      <CustomArrowTopUPbottomDown id={id} delay={delay} start={[start[0] -1, start[1], start[2]]} end={[end[0]- 1, end[1], end[2]]}  type={type} />
+      <CustomArrowTopUPbottomDown id={id} delay={delay} start={[start[0] +1, start[1]+2, start[2]]} end={[end[0]+1, end[1]+2, end[2]]}  type={type} />
+      <CustomArrowTopUPbottomDown id={id} delay={delay} start={[start[0] -1, start[1]+2, start[2]]} end={[end[0]- 1, end[1]+2, end[2]]}  type={type} />
+      
+      <CustomArrowTopUPbottomDown id={`${id}_1`} delay={delay}  start={[-start[0] -1, start[1], start[2]]} end={[-end[0]-1, end[1], end[2]]} type={'top_DOWN'} />
+  <CustomArrowTopUPbottomDown id={`${id}_2`} delay={delay} start={[-start[0] -1, start[1]+2, start[2]]} end={[-end[0]-1, end[1]+2, end[2]]} type={'top_DOWN'} />
+       
+            
+      <CustomArrowTopUPbottomDown id={`${id}_3`}delay={delay} start={[-start[0]+1, start[1]+2, start[2]]} end={[-end[0]+1, end[1]+2, end[2]]} type={'top_DOWN'} />
+      <CustomArrowTopUPbottomDown id={`${id}_4`} delay={delay } start={[-start[0] + 1, start[1], start[2]]} end={[-end[0] + 1, end[1], end[2]]} type={'top_DOWN'} /> 
+  
+    </>
+  );
+  
--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { Sphere,Circle,DoubleArrow,Arrow,MyText,CameraController } from './components';
+import {CustomArrowHorizontal, Circle } from './components';
+import { createSphere, createArrowHorizontal, createArrowVertical, createArrowDiagonal, createBottomUpTopDown } from './Animation_center';
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
-import * as THREE from 'three'
+
 
 export default function App() {
   return (
@@ -11,52 +12,35 @@ export default function App() {
       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
 
       <group position={[-1, 0, 0]}>
-        <Sphere id="entity1" position={[-4, 2, 0]} delay={1000} />
-        <MyText position={[-5, 3, 0]} fontSize={1 / 2} delay={1500}>
-          entity1
-        </MyText>
-        <MyText position={[-3, 3, 0]} fontSize={1 / 2} delay={4500}>
-          relation
-        </MyText>
+        {createSphere("entity1", [-4, 2, 0], 1000)}
+        {createSphere("entity2", [-2, 2, 0], 2000)}
 
-        <Sphere id="entity2" position={[-2, 2, 0]} delay={2000} />
-        <Sphere id="entity3" position={[-4, 0, 0]} delay={5000} />
-        <Sphere id="entity4" position={[-2, 0, 0]} delay={5000} />
-        <DoubleArrow id="relation_arrow1" from={new THREE.Vector3(-2.5, 2, 0)} to={new THREE.Vector3(-3.5, 2, 0)} delay={4000} />
-        <DoubleArrow id="relation_arrow2" from={new THREE.Vector3(-2, 0.5, 0)} to={new THREE.Vector3(-2, 1.5, 0)} delay={5000} />
-        <DoubleArrow id="relation_arrow3" from={new THREE.Vector3(-4, 0.5, 0)} to={new THREE.Vector3(-4, 1.5, 0)} delay={5000} />
-        <DoubleArrow id="relation_arrow4" from={new THREE.Vector3(-2.3, 0.4, 0)} to={new THREE.Vector3(-3.6, 1.7, 0)} delay={5000} />
-        <DoubleArrow id="relation_arrow5" from={new THREE.Vector3(-3.5, 0.3, 0)} to={new THREE.Vector3(-2.4, 1.7, 0)} delay={5000} />
-        <DoubleArrow id="relation_arrow6" from={new THREE.Vector3(-2.5, 0, 0)} to={new THREE.Vector3(-3.5, 0, 0)} delay={5000} />
-        <Circle id="circle1" position={[-3.5, 1, -1.5]} delay={6000} />
+        
+        {createSphere("entity3", [-4, 0, 0], 5000)}
+        {createSphere("entity4", [-2, 0, 0], 5000)}
+        <Circle id="circle1" position={[-3, 1, -1.5]} delay={6000} />
 
-        <Arrow from={new THREE.Vector3(-2, 0, -1.5)} to={new THREE.Vector3(-2, 0, -0.5)} delay={8000} />
-        <Arrow from={new THREE.Vector3(-2, 2, -1.5)} to={new THREE.Vector3(-2, 2, -0.5)} delay={8000} />
-        <Arrow from={new THREE.Vector3(-4, 2, -1.5)} to={new THREE.Vector3(-4, 2, -0.5)} delay={8000} />
-        <Arrow from={new THREE.Vector3(-4, 0, -1.5)} to={new THREE.Vector3(-4, 0, -0.5)} delay={8000} />
+        {createArrowHorizontal("arrow1 and arrow4", 3000, [-4, 2, 0], [-2, 2, 0])}
+        {createArrowVertical("arrow2 and arrow3", 5000, [-4, 0.5, 0], [-4, 1.5, 0])}
+        {createArrowDiagonal("arrow5", 5000, [-2, 0, 0], [-4, 2, 0])}
+        {/* ... */}
       </group>
 
       <group position={[1, 0, 0]}>
-        <Sphere id="entity1_1" position={[2, 0, 0]} delay={8000} />
-        <Sphere id="entity1_2" position={[4, 2, 0]} delay={8000} />
-        <Sphere id="entity1_3" position={[4, 0, 0]} delay={8000} />
-        <Sphere id="entity1_4" position={[2, 2, 0]} delay={8000} />
-        <DoubleArrow id="relation_arrow1_1" from={new THREE.Vector3(2.3, 0.4, 0)} to={new THREE.Vector3(3.6, 1.7, 0)} delay={8000}  />
-        <DoubleArrow id="relation_arrow1_2" from={new THREE.Vector3(2, 0.5, 0)} to={new THREE.Vector3(2, 1.5, 0)} delay={8000} />
-        <DoubleArrow id="relation_arrow1_3" from={new THREE.Vector3(4, 0.5, 0)} to={new THREE.Vector3(4, 1.5, 0)} delay={8000} />
-        <DoubleArrow id="relation_arrow1_4" from={new THREE.Vector3(3.5, 0.3, 0)} to={new THREE.Vector3(2.4, 1.7, 0)} delay={8000} />
-        <DoubleArrow id="relation_arrow1_5" from={new THREE.Vector3(2.5, 0, 0)} to={new THREE.Vector3(3.5, 0, 0)} delay={8000} />
-        <DoubleArrow id="relation_arrow1_6" from={new THREE.Vector3(2.5, 2, 0)} to={new THREE.Vector3(3.5, 2, 0)} delay={8000} />
-        <Circle id="circle1_1" position={[3.5, 1, -1.5]} delay={8000} />
+        {createSphere("entity1_1", [2, 0, 0], 8000)}
+        {createSphere("entity1_2", [4, 2, 0], 8000)}
+        {createSphere("entity1_3", [4, 0, 0], 8000)}
+        {createSphere("entity1_4", [2, 2, 0], 8000)}
 
-        <Arrow from={new THREE.Vector3(2, 0, -0.5)} to={new THREE.Vector3(2, 0, -1.5)} delay={10000} />
-        <Arrow from={new THREE.Vector3(2, 2, -0.5)} to={new THREE.Vector3(2, 2, -1.5)} delay={10000} />
-        <Arrow from={new THREE.Vector3(4, 2, -0.5)} to={new THREE.Vector3(4, 2, -1.5)} delay={10000} />
-        <Arrow from={new THREE.Vector3(4, 0, -0.5)} to={new THREE.Vector3(4, 0, -1.5)} delay={10000} />
+        
+
+        <Circle id="circle1_1" position={[3, 1, -1.5]} delay={8000} />
+        {/* ... */}
       </group>
-      <CameraController />
-      <DoubleArrow id="relation_arrow1_6" from={new THREE.Vector3(-1, 1, -1.5)} to={new THREE.Vector3(1, 1, -1.5)} delay={11000} />
-    
+
+      <CustomArrowHorizontal id="arrow5" delay={11000} start={[-1, 1, -1.5]} end={[1, 1, -1.5]} />
+
+      {createBottomUpTopDown("arrow6", 8000, [4, 0, -0.5], [4, 0, -1.5])}
       <OrbitControls />
     </Canvas>
   )
