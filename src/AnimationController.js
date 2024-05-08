0e37e750995d0f9e42b0b79fa19cfb2828c668bd
Amit Digga <diggaamitoo7@gmail.com>
2024-05-08 13:36:54 +0530

    Fixed camera

--- src/AnimationController.js
+++ src/AnimationController.js
@@ -46,7 +46,7 @@ export function AnimationController({ children }) {
             [2, 'emergent1.text', { visible: false }],
             [1, 'emergent2', { visible: true }],
             [0, 'emergent2.Circle', { duration: 1, variant: "visible", opacity: 0.5, visible: true }],
-            [0, 'camera', { position: [0, -20, 10] }],
+            [0, 'camera', { position: [0, -25, 10] }],
             [0.5, 'inter_emergent', { visible: true }],
             [1, 'emergent2.text', { visible: true, text: "Bottom-up" }],
             [1, 'emergent2.causation', { visible: true }],
