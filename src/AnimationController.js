1f34133740e8815728f1c3b1347830281b19d403
Amit Digga <diggaamitoo7@gmail.com>
2024-05-13 13:36:02 +0530

    Using States for Emergent Entity 1

--- src/AnimationController.js
+++ src/AnimationController.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
-import useStore from './useStore';
 import * as THREE from 'three';
+import useStore from './useStore';
 
 export function AnimationController({ children }) {
     const { batchUpdateAnimationStates, updateAnimationState } = useStore(state => ({
@@ -25,28 +25,28 @@ export function AnimationController({ children }) {
             'emergent2.label': { visible: false },
             'emergent2.causation': { visible: false },
         });
-        
+
         // Delay, id, animationState
         const animationSteps = [
-            [1,   'emergent1', { variant: "oneSphere-details", }],
-            [1,   'emergent1', { variant: "twoSphere", why: "Showing second sphere" }],
-            [1,   'emergent1', { variant: "relation" }],
-            [1,   'emergent1', { variant: "allRelations" }],
-            [0,   'emergent1.label2', { text: "Accumulation" }],
-            [1,   'emergent1', { variant: "accumulation-description" }],
-            [1,   'emergent1', { variant: "accumulation-description-end" }],
-            [1,   'emergent1.Circle', { duration: 1, variant: "visible", opacity: 0.5, visible: true }],
-            [1,   'emergent1.label', { visible: true }],
-            [2,   'emergent1.label', { visible: false }],
-            [1,   'emergent2', { visible: true }],
-            [0,   'emergent2.Circle', { duration: 1, variant: "visible", opacity: 0.5, visible: true }],
-            [0,   'camera', { position: [0, -30, 10], duration: 2000 }],
-            [2,   'emergent1.label', { visible: true, text: "Bottom-up" }],
-            [1,   'emergent1.causation', { visible: true }],
+            [1, 'emergent1', { variant: "oneSphere-details", }],
+            [1, 'emergent1', { variant: "twoSphere", why: "Showing second sphere" }],
+            [1, 'emergent1', { variant: "relation" }],
+            [1, 'emergent1', { variant: "allRelations" }],
+            // [0, 'emergent1.label2', { text: "Accumulation" }],
+            [1, 'emergent1', { variant: "accumulation-description" }],
+            [1, 'emergent1', { variant: "accumulation-description-end" }],
+            [1, 'emergent1.Circle', { duration: 1, variant: "visible", opacity: 0.5, visible: true }],
+            // [1, 'emergent1.label', { visible: true }],
+            // [2, 'emergent1.label', { visible: false }],
+            [1, 'emergent2', { visible: true }],
+            [0, 'emergent2.Circle', { duration: 1, variant: "visible", opacity: 0.5, visible: true }],
+            [0, 'camera', { position: [0, -30, 10], duration: 2000 }],
+            [2, 'emergent1.label', { visible: true, text: "Bottom-up" }],
+            [1, 'emergent1.causation', { visible: true }],
             [0.5, 'inter_emergent', { visible: true }],
-            [1,   'emergent2.label', { visible: true, text: "Top-down" }],
-            [1,   'emergent2.causation', { visible: true }],
-            [1,   'emergent1', { variant: "moved", offset: new THREE.Vector3(5,5,5) }],
+            [1, 'emergent2.label', { visible: true, text: "Top-down" }],
+            [1, 'emergent2.causation', { visible: true }],
+            [1, 'emergent1', { variant: "moved", offset: new THREE.Vector3(5, 5, 5) }],
 
         ];
 
--- src/animationComponents/EmergentEntity.js
+++ src/animationComponents/EmergentEntity.js
@@ -4,11 +4,105 @@ import useStore from '../useStore';
 import withAnimationState from '../withAnimationState';
 import { Circle, CustomText, DynamicDoubleArrow, FatArrow, Sphere } from './';
 
+
+const defaultInnerState = (id) => ({
+  [`${id}.Sphere1`]: { visible: false },
+  [`${id}.Sphere2`]: { visible: false },
+  [`${id}.Sphere3`]: { visible: false },
+  [`${id}.Sphere4`]: { visible: false },
+  [`${id}.Circle`]: { visible: false },
+  [`${id}.label`]: { visible: false },
+  [`${id}.label2`]: { visible: false },
+  [`${id}.causation`]: { visible: false },
+  [`${id}.relations1`]: { visible: false },
+  [`${id}.relations2`]: { visible: false },
+  [`${id}.relations3`]: { visible: false },
+  [`${id}.relations4`]: { visible: false },
+  [`${id}.relations5`]: { visible: false },
+  [`${id}.relations6`]: { visible: false },
+})
+
+function allSphereVisible(id, visible) {
+  return {
+    [`${id}.Sphere1`]: { visible },
+    [`${id}.Sphere2`]: { visible },
+    [`${id}.Sphere3`]: { visible },
+    [`${id}.Sphere4`]: { visible },
+  }
+}
+function allRelationsVisible(id, visible) {
+  return {
+    [`${id}.relations1`]: { visible },
+    [`${id}.relations2`]: { visible },
+    [`${id}.relations3`]: { visible },
+    [`${id}.relations4`]: { visible },
+    [`${id}.relations5`]: { visible },
+    [`${id}.relations6`]: { visible },
+  }
+}
+
+function allVisible(id, visible) {
+  return {
+    ...allSphereVisible(id, visible),
+    ...allRelationsVisible(id, visible),
+    [`${id}.Circle`]: { visible },
+    [`${id}.label`]: { visible },
+    [`${id}.label2`]: { visible },
+    [`${id}.causation`]: { visible },
+  }
+}
+
+
+function getAnimationUpdates(id, variant) {
+  switch (variant) {
+    case 'oneSphere':
+      return {
+        ...allVisible(id, false),
+        [`${id}.Sphere1`]: { visible: true }
+      };
+    // case 'oneSphere-details':
+    //   return {
+    //     [`${id}.Sphere1.label`]: { variant: 'visible', duration: 0.5 },
+    //   };
+    case 'twoSphere':
+      return {
+        ...allVisible(id, false),
+        [`${id}.Sphere1`]: { visible: true },
+        [`${id}.Sphere2`]: { visible: true },
+        // [`${id}.Sphere1.label`]: { variant: 'hidden', duration: 0.5 },
+      };
+    case 'relation':
+      return {
+        ...allVisible(id, false),
+        [`${id}.Sphere1`]: { visible: true },
+        [`${id}.Sphere2`]: { visible: true },
+        [`${id}.relations1`]: { visible: true },
+      };
+    case 'allRelations':
+      return {
+        ...allVisible(id, true),
+        [`${id}.Circle`]: { visible: false }
+      };
+    // case 'accumulation-description':
+    //   return {
+    //     [`${id}.label2`]: { visible: true },
+    //   };
+    // case 'accumulation-description-end':
+    //   return {
+    //     [`${id}.label2`]: { visible: false },
+    //   };
+    default:
+      // Optionally handle the default case
+      return {};
+  }
+}
+
+
 const EmergentEntity = React.forwardRef(({ id, animationState, ...props }, ref) => {
 
   const { updateAnimationState, batchUpdateAnimationStates } = useStore();
 
-  const { radius, visible, offset= new THREE.Vector3(0,0,0) } = animationState;
+  const { radius, visible, offset = new THREE.Vector3(0, 0, 0) } = animationState;
   // Clone the position to avoid mutating the original data.
   const position = new THREE.Vector3().copy(animationState.position);
   position.add(offset);
@@ -22,66 +116,7 @@ const EmergentEntity = React.forwardRef(({ id, animationState, ...props }, ref) 
 
   // Effect to update child component's animation state based on the this variant
   useEffect(() => {
-    switch (animationState.variant) {
-      case 'oneSphere':
-        batchUpdateAnimationStates({
-          [`${id}.Circle`]: { visible: false },
-          [`${id}.causation`]: { visible: false },
-          [`${id}.Sphere2`]: { visible: false },
-          [`${id}.Sphere3`]: { visible: false },
-          [`${id}.Sphere4`]: { visible: false },
-          [`${id}.label`]: { visible: false },
-        });
-        break;
-      case 'oneSphere-details':
-        batchUpdateAnimationStates({
-          [`${id}.Sphere1.label`]: { variant: 'visible', duration: .5 },
-        });
-        break;
-      case 'twoSphere':
-        batchUpdateAnimationStates({
-          [`${id}.Sphere2`]: { visible: true },
-          [`${id}.Sphere1.label`]: { variant: 'hidden', duration: .5 },
-        });
-        break;
-      case 'relation':
-        batchUpdateAnimationStates({
-          [`${id}.relations1`]: { visible: true },
-          [`${id}.relations2`]: { visible: false },
-          [`${id}.relations3`]: { visible: false },
-          [`${id}.relations4`]: { visible: false },
-          [`${id}.relations5`]: { visible: false },
-          [`${id}.relations6`]: { visible: false },
-        });
-        break;
-      case 'allRelations':
-        batchUpdateAnimationStates({
-          [`${id}.relations1`]: { visible: true },
-          [`${id}.relations2`]: { visible: true },
-          [`${id}.relations3`]: { visible: true },
-          [`${id}.relations4`]: { visible: true },
-          [`${id}.relations5`]: { visible: true },
-          [`${id}.relations6`]: { visible: true },
-          [`${id}.Sphere1`]: { visible: true },
-          [`${id}.Sphere2`]: { visible: true },
-          [`${id}.Sphere3`]: { visible: true },
-          [`${id}.Sphere4`]: { visible: true },
-        });
-        break;
-      case 'accumulation-description':
-        batchUpdateAnimationStates({
-          [`${id}.label2`]: { visible: true },
-        });
-        break;
-      case 'accumulation-description-end':
-        batchUpdateAnimationStates({
-          [`${id}.label2`]: { visible: false },
-        });
-        break;
-      default:
-        // Handle default case if needed
-        break;
-    }
+    batchUpdateAnimationStates(getAnimationUpdates(id, animationState.variant))
   }, [animationState.variant, batchUpdateAnimationStates, id]);
 
   // Define animation variants
@@ -112,8 +147,8 @@ const EmergentEntity = React.forwardRef(({ id, animationState, ...props }, ref) 
   return (
     // We set a mesh for this object so we can get a ref for DynamicDoubleArrow above this component
     <group ref={ref} position={position} visible={visible} userData={{ meshId: `${id}.Circle` }} >
-      <CustomText 
-        id={`${id}.label`} 
+      <CustomText
+        id={`${id}.label`}
         initialState={{
           visible: false,
           position: labelPosition,
@@ -128,22 +163,22 @@ const EmergentEntity = React.forwardRef(({ id, animationState, ...props }, ref) 
           scale: 0.3
         }}
       />
-      <Circle id={`${id}.Circle`} initialState={{radius: radius}} />
-      <Sphere id={`${id}.Sphere1`} initialState={{position: spherePosition1, radius: sphereRadius}} />
-      <Sphere id={`${id}.Sphere2`} initialState={{position: spherePosition2, radius: sphereRadius}} />
-      <Sphere id={`${id}.Sphere3`} initialState={{position: spherePosition3, radius: sphereRadius}} />
-      <Sphere id={`${id}.Sphere4`} initialState={{position: spherePosition4, radius: sphereRadius}} />
+      <Circle id={`${id}.Circle`} initialState={{ radius: radius }} />
+      <Sphere id={`${id}.Sphere1`} initialState={{ position: spherePosition1, radius: sphereRadius }} />
+      <Sphere id={`${id}.Sphere2`} initialState={{ position: spherePosition2, radius: sphereRadius }} />
+      <Sphere id={`${id}.Sphere3`} initialState={{ position: spherePosition3, radius: sphereRadius }} />
+      <Sphere id={`${id}.Sphere4`} initialState={{ position: spherePosition4, radius: sphereRadius }} />
       <group visible={relationsAnimationState.visible}>
-        <DynamicDoubleArrow id={`${id}.relations1`} initialState={{visible: false, fromId: `${id}.Sphere1`, toId: `${id}.Sphere2`}} />
-        <DynamicDoubleArrow id={`${id}.relations2`} initialState={{visible: false, fromId: `${id}.Sphere3`, toId: `${id}.Sphere4`}} />
-        <DynamicDoubleArrow id={`${id}.relations3`} initialState={{visible: false, fromId: `${id}.Sphere1`, toId: `${id}.Sphere3`}} />
-        <DynamicDoubleArrow id={`${id}.relations4`} initialState={{visible: false, fromId: `${id}.Sphere2`, toId: `${id}.Sphere4`}} />
-        <DynamicDoubleArrow id={`${id}.relations5`} initialState={{visible: false, fromId: `${id}.Sphere1`, toId: `${id}.Sphere4`}} />
-        <DynamicDoubleArrow id={`${id}.relations6`} initialState={{visible: false, fromId: `${id}.Sphere2`, toId: `${id}.Sphere3`}} />
+        <DynamicDoubleArrow id={`${id}.relations1`} initialState={{ visible: false, fromId: `${id}.Sphere1`, toId: `${id}.Sphere2` }} />
+        <DynamicDoubleArrow id={`${id}.relations2`} initialState={{ visible: false, fromId: `${id}.Sphere3`, toId: `${id}.Sphere4` }} />
+        <DynamicDoubleArrow id={`${id}.relations3`} initialState={{ visible: false, fromId: `${id}.Sphere1`, toId: `${id}.Sphere3` }} />
+        <DynamicDoubleArrow id={`${id}.relations4`} initialState={{ visible: false, fromId: `${id}.Sphere2`, toId: `${id}.Sphere4` }} />
+        <DynamicDoubleArrow id={`${id}.relations5`} initialState={{ visible: false, fromId: `${id}.Sphere1`, toId: `${id}.Sphere4` }} />
+        <DynamicDoubleArrow id={`${id}.relations6`} initialState={{ visible: false, fromId: `${id}.Sphere2`, toId: `${id}.Sphere3` }} />
       </group>
       {
         animationState.causation === "bottomup" ?
-        causationArrows(`${id}.causation`, new THREE.Vector3(0, 0, -(causationLength - sphereRadius)), new THREE.Vector3(0, 0, -causationLength * 0.05)) : causationArrows(`${id}.causation`, new THREE.Vector3(0, 0, -causationLength * 0.05), new THREE.Vector3(0, 0, -(causationLength - sphereRadius)))
+          causationArrows(`${id}.causation`, new THREE.Vector3(0, 0, -(causationLength - sphereRadius)), new THREE.Vector3(0, 0, -causationLength * 0.05)) : causationArrows(`${id}.causation`, new THREE.Vector3(0, 0, -causationLength * 0.05), new THREE.Vector3(0, 0, -(causationLength - sphereRadius)))
       }
     </group>
   );
