e714e2bb13e5abd7debf5770e1cf3df8691effd8
mohAmineBrs <bourouis.mohammed.amine@gmail.com>
2024-05-20 19:16:41 +0100

    feat: add SceneJoints for joined sphere in circle form

--- src/SceneManager.js
+++ src/SceneManager.js
@@ -1,53 +1,59 @@
-import React, { useState, useEffect } from 'react';
-import useStore from './useStore';
-import SceneOne from './scenes/SceneOne';
-import SceneTwo from './scenes/SceneTwo';
-import SceneThree from './scenes/SceneThree';
+import React, { useState, useEffect } from 'react'
+import useStore from './useStore'
+import SceneOne from './scenes/SceneOne'
+import SceneTwo from './scenes/SceneTwo'
+import SceneThree from './scenes/SceneThree'
+import SceneJoints from './scenes/SceneJoints'
 
 const SceneManager = () => {
-  const currentScene = useStore((state) => state.currentScene);
-  const reloadScene = useStore((state) => state.reloadScene);
-  const setReloadScene = useStore((state) => state.setReloadScene);
-  const setUsePhysics = useStore((state) => state.setUsePhysics);
-  const [key, setKey] = useState(0);
-  const [sceneInfo, setSceneInfo] = useState({ sceneComponent: null, isOrthographic: true });
+  const currentScene = useStore((state) => state.currentScene)
+  const reloadScene = useStore((state) => state.reloadScene)
+  const setReloadScene = useStore((state) => state.setReloadScene)
+  const setUsePhysics = useStore((state) => state.setUsePhysics)
+  const [key, setKey] = useState(0)
+  const [sceneInfo, setSceneInfo] = useState({ sceneComponent: null, isOrthographic: true })
 
   useEffect(() => {
     // Update the key whenever the scene reloads
-    setKey((prevKey) => prevKey + 1);
-    setReloadScene(false);
-  }, [reloadScene]);
+    setKey((prevKey) => prevKey + 1)
+    setReloadScene(false)
+  }, [reloadScene])
 
   useEffect(() => {
-    if (!reloadScene) return;
+    if (!reloadScene) return
 
-    let sceneComponent;
-    let isOrthographic = false;
+    let sceneComponent
+    let isOrthographic = false
 
     switch (currentScene) {
       case 'SceneOne':
-        sceneComponent = <SceneOne />;
-        isOrthographic = true;
-        setUsePhysics(false);
-        break;
+        sceneComponent = <SceneOne />
+        isOrthographic = true
+        setUsePhysics(false)
+        break
       case 'SceneTwo':
-        sceneComponent = <SceneTwo />;
-        isOrthographic = true;
-        setUsePhysics(true);
-        break;
+        sceneComponent = <SceneTwo />
+        isOrthographic = true
+        setUsePhysics(true)
+        break
       case 'SceneThree':
-        sceneComponent = <SceneThree />;
-        isOrthographic = true;
-        setUsePhysics(true);
-        break;
+        sceneComponent = <SceneThree />
+        isOrthographic = true
+        setUsePhysics(true)
+        break
+      case 'SceneJoints':
+        sceneComponent = <SceneJoints />
+        isOrthographic = true
+        setUsePhysics(true)
+        break
       default:
-        throw new Error(`Unknown Scene: ${currentScene}`);
+        throw new Error(`Unknown Scene: ${currentScene}`)
     }
 
-    setSceneInfo({ sceneComponent, isOrthographic });
-  }, [currentScene, setUsePhysics]);
+    setSceneInfo({ sceneComponent, isOrthographic })
+  }, [currentScene, setUsePhysics])
 
-  return { ...sceneInfo, key };
-};
+  return { ...sceneInfo, key }
+}
 
-export default SceneManager;
+export default SceneManager
--- src/SceneSelector.js
+++ src/SceneSelector.js
@@ -1,25 +1,26 @@
 // SceneSelector.js
-import React from 'react';
-import useStore from './useStore'; // Make sure the path is correct
+import React from 'react'
+import useStore from './useStore' // Make sure the path is correct
 
 const SceneSelector = () => {
-  const setCurrentScene = useStore((state) => state.setCurrentScene);
-  const setReloadScene = useStore((state) => state.setReloadScene);
-  const clearAllAnimationStates = useStore((state) => state.clearAllAnimationStates);
+  const setCurrentScene = useStore((state) => state.setCurrentScene)
+  const setReloadScene = useStore((state) => state.setReloadScene)
+  const clearAllAnimationStates = useStore((state) => state.clearAllAnimationStates)
 
   const initializeScene = (sceneName) => {
-    clearAllAnimationStates(); // Clear all animation states
-    setCurrentScene(sceneName); // Set the current scene
+    clearAllAnimationStates() // Clear all animation states
+    setCurrentScene(sceneName) // Set the current scene
     setReloadScene(true)
-  };
+  }
 
   return (
     <div style={{ zIndex: 10 }}>
       <button onClick={() => initializeScene('SceneOne')}>Go to Scene One</button>
       <button onClick={() => initializeScene('SceneTwo')}>Go to Scene Two</button>
       <button onClick={() => initializeScene('SceneThree')}>Go to Scene Three</button>
+      <button onClick={() => initializeScene('SceneJoints')}>Go to Scene Joints</button>
     </div>
-  );
-};
+  )
+}
 
-export default SceneSelector;
+export default SceneSelector
