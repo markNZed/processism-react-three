888850ef7bb04530a55ed356a04a13c49f827664
Mark <NA>
2024-07-31 17:05:07 +0000

    Comments

--- src/animationComponents/Complexity/CompoundEntity.js
+++ src/animationComponents/Complexity/CompoundEntity.js
@@ -34,9 +34,7 @@ import useStore from '../../useStore'
 */
 
 // Right click on particle could show top blob in the same color
-// Test useAnimateImpulses, 
-// Test useAnimateJoints
-//   Not using center - should pass that in ?
+// Test useAnimateImpulses
 
 const CompoundEntity = React.memo(React.forwardRef(({ id, initialPosition = [0, 0, 0], radius, debug, config, outer = {}, ...props }, ref) => {
 
