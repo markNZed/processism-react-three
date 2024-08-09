a38343d7572b7f5245ac4a75377bc3ee45e92b78
Mark <NA>
2024-08-09 14:53:45 +0000

    minor

--- src/scenes/SceneThree.js
+++ src/scenes/SceneThree.js
@@ -101,8 +101,6 @@ function SceneThree() {
 
                         <MyAxesHelper />
 
-                        <DynamicSpeechSphere />
-
                     </>
                 </Physics>
             </AnimationController>
